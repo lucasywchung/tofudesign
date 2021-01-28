5d9666a7f9e321666f51602a4937e0160327ee2b
lucasywchung <78174272+lucasywchung@users.noreply.github.com>
2021-01-28 20:41:56 +0100

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+//SCROLL REVEAL
+
+//LANDING PAGE
+//Heading Masthead
+ScrollReveal().reveal('.masthead');
+ScrollReveal().reveal('.masthead-subheading', {delay:300});
+
+//POR QUE TOFU SECTION
+ScrollReveal().reveal('.page-section');
+ScrollReveal().reveal('.icon', {delay:100});
+ScrollReveal().reveal('.section-heading', {delay:300});
+ScrollReveal().reveal('.title', {delay:300});
+ScrollReveal().reveal('.title-subheading', {delay:500});
+
+//QUIENES SOMOS SECTION
+ScrollReveal().reveal('.page-section-2');
+ScrollReveal().reveal('.avatar');
+
+//CONTACTO SECTION
+ScrollReveal().reveal('.page-section-3', {delay:300});
+
+//BUTTONS
+ScrollReveal().reveal('.TofuColorBtn', {delay:300});
+ScrollReveal().reveal('.TofuColorBtn-2', {delay:300});
+ScrollReveal().reveal('.green-btn', {delay:300});
+ScrollReveal().reveal('.yellow-btn', {delay:300});
+
+
+//HOME
+
+//ABOUT
+
+//PROYECTOS
+
+//CONTACTO
+
+
+/* AOS animation */
+
+AOS.init();
